c4e2d6058a34f540167fead52b196aee169811f9
Michael Collins <michael-collins@users.noreply.github.com>
2024-02-26 00:03:24 -0500

    component updates

--- 3d-pathways/src/components/AccordionComponent.vue
+++ 3d-pathways/src/components/AccordionComponent.vue
@@ -1,99 +1,62 @@
 <template>
-  <div  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-10">
-    <div class="flex flex-col p-3 border rounded-lg border-base-300 bg-base-200">
-
-
-        
-        <div v-for="record in records" :key="record.id" class="flex flex-col p-3 border rounded-lg border-base-300 bg-base-200">
-          <div class="text-base-content text-xl p-2">
-            <div @click="navigateToRecordDetail(record.id)" class="cursor-pointer">
-            {{ record.fields.Name }}
-            </div>
-          </div>
-          <div class="join join-vertical w-full">
-          <div class="collapse collapse-arrow join-item border border-base-300">
-            <input type="checkbox" :id="'accordion-learning-objectives' + record.id" class="peer" />
-            <div class="collapse-title text-md font-medium ">
-              Learning Objectives
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ record.fields['learning objectives'] }}</p>
-              
-            </div>
-          </div>
-          <div class="collapse collapse-arrow join-item border border-base-300">
-            <input type="checkbox" :id="'accordion-exercises' + record.id" class="peer" />
-            <div class="collapse-title text-md font-medium ">
-              Exercises
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ record.fields.exercises }}</p>
-              
-            </div>
-          </div>
-          <div class="collapse collapse-arrow join-item border border-base-300">
-            <input type="checkbox" :id="'accordion-learning-fields-of-practice' + record.id" class="peer" />
-            <div class="collapse-title text-md font-medium ">
-              Fields of Practice
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ record.fields['fields of practice'] }}</p>
-              
-            </div>
-          </div>
-          <div class="collapse collapse-arrow join-item border border-base-300">
-            <input type="checkbox" :id="'accordion-learning-examples' + record.id" class="peer" />
-            <div class="collapse-title text-md font-medium ">
-              Examples
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ record.fields.examples }}</p>
-              
-            </div>
-          </div>
-          <div class="collapse collapse-arrow join-item border border-base-300">
-            <input type="checkbox" :id="'accordion-learning-competencies' + record.id" class="peer" />
-            <div class="collapse-title text-md font-medium ">
-              Competencies
-            </div>
-            <div class="collapse-content"> 
-              <p>{{ record.fields.competencies }}</p>
-              
-            </div>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-10">
+    <AccordionItem v-for="record in records" :key="record.id" :initiallyOpen="true">
+      <template v-slot:header>
+        <div class="navbar">
+          <h2 class=" flex-1">{{ record.fields.Name }}
+          
+        </h2>
+        <button class="btn flex-none" @click="navigateToRecordDetail(record.id)">
+          View
+            <svg class="h-5 w-5 text-slate-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />  <line x1="10" y1="14" x2="20" y2="4" />  <polyline points="15 4 20 4 20 9" /></svg>
+        </button>
+      </div>
+      </template>
+      <template v-slot:body>
+        <div class="collapse collapse-arrow border border-base-300 my-2" v-for="detail in recordDetails" :key="detail.key">
+          <input type="checkbox" :id="'accordion-' + detail.key + '-' + record.id" class="peer" />
+          <label :for="'accordion-' + detail.key + '-' + record.id" class="collapse-title text-md font-medium">
+            {{ detail.title }}
+          </label>
+          <div class="collapse-content">
+            <p>{{ record.fields[detail.key] }}</p>
           </div>
         </div>
-      </div>
-    </div>
-    </div>
-    
-          
-          
-          
+      </template>
+    </AccordionItem>
+  </div>
+</template>
 
-  </template>
-  
-  <script>
-  // import AirtableService from '@/services/AirtableService';
-  
-  export default {
-    props: ['records'],
-    
-    mounted() {
-      
-      
+<script>
+import AccordionItem from './AccordionItem.vue';
+
+export default {
+  components: {
+    AccordionItem,
+  },
+  props: ['records'],
+  computed: {
+    recordDetails() {
+      return [
+        { key: 'learning objectives', title: 'Learning Objectives' },
+        { key: 'exercises', title: 'Exercises' },
+        { key: 'fields of practice', title: 'Fields of Practice' },
+        { key: 'examples', title: 'Examples' },
+        { key: 'competencies', title: 'Competencies' },
+      ];
     },
-    methods: {
-      navigateToRecordDetail(recordId) {
-  this.$router.push({ 
-    name: 'RecordDetail', 
-    params: { id: recordId }
-  });
-},
-      toggle(id) {
-        this.activeId = this.activeId === id ? null : id;
-      }
-    }
-  };
-  </script>
-
-  
+  },
+  methods: {
+    shouldItemBeOpen(record) {
+    // Your logic here, for example:
+    return record.isOpenByDefault;
+  },
+    navigateToRecordDetail(recordId) {
+      this.$router.push({
+        name: 'RecordDetail',
+        params: { id: recordId },
+      });
+    },
+  },
+};
+</script>
---
+++ 3d-pathways/src/components/AccordionItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="['flex flex-col p-3 border mx-2 rounded-lg border-base-300 bg-base-200', { 'collapse-open': isOpen }]">
+      <div @click="toggle" class="cursor-pointer text-base-content text-xl p-2">
+        <slot name="header"></slot>
+      </div>
+      <div v-show="isOpen" class="join join-vertical w-full">
+        <slot name="body"></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      // Prop to control the initial or direct state of the accordion item
+      initiallyOpen: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        // Use the prop to set the initial state
+        isOpen: this.initiallyOpen,
+      };
+    },
+    methods: {
+      toggle() {
+        this.isOpen = !this.isOpen;
+      },
+    },
+  };
+  </script>
+  
