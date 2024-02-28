cc23a434b95f4015a76cd4fd445f09e79a7615a2
Michael Collins <michael-collins@users.noreply.github.com>
2024-02-28 09:22:40 -0500

    styles

--- 3d-pathways/src/views/RecordDetail.vue
+++ 3d-pathways/src/views/RecordDetail.vue
@@ -5,7 +5,7 @@
       <div class="navbar bg-base-100">
         <div class=" text-sm breadcrumbs">
           <ul class="p-2 bg-base-100 rounded-t-none">
-            <li class="btn btn-outline uppercase text-xs" @click="navigateToHomePage">
+            <li class="btn btn-ghost hover:btn-outline uppercase text-xs" @click="navigateToHomePage">
               
               <svg class="h-5 w-5 "  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="4" />  <line x1="1.05" y1="12" x2="7" y2="12" />  <line x1="17.01" y1="12" x2="22.96" y2="12" /></svg>
 
